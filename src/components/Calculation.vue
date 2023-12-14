adbfee1f5c87399a30f657611eea407ae893b789
Volperter <florianellhof020205@gmail.com>
2023-12-14 17:00:57 +0100

    updated input validation

--- src/components/Calculation.vue
+++ src/components/Calculation.vue
@@ -1,8 +1,7 @@
 <template>
-
-
-  <form @submit.prevent="calculateEmission()" class="block p-4 border shadow"> <!-- bg-gray-600 -->
+  <form @submit.prevent="calculateEmission()" novalidate class="block bg-gray-600 p-4">
     <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+
       <div class="col-span-2 text-white flex items-center justify-end">
         <!--
         <input v-model="transportMediumCustomConsumption" @click="resetForm" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
@@ -11,17 +10,20 @@
         <label class="relative inline-flex items-center cursor-pointer">
           <input v-model="transportMediumCustomConsumption" @click="resetForm" type="checkbox" value="" class="sr-only peer">
           <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-          <span class="ms-3 text-sm font-medium text-gray-500 dark:text-gray-300">Verbrauch manuell eingeben?</span>
+          <span class="ms-3 text-sm font-medium dark:text-gray-300">Verbrauch manuell eingeben?</span>
         </label>
       </div>
 
+
       <div class="col-span-2 flex justify-center lg:flex-row flex-col ">
+
         <div class="relative">
           <input v-model="startLocation" type="text" name="startLocation" id="startLocation" class="pl-10 rounded-lg lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-600 focus:border-green-600 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Magdeburg" required="">
           <div class="absolute inset-y-4 start-0 top-0 flex items-center ps-3.5 pt-0 pointer-events-none">
             <i class="fa-solid fa-location-dot"></i>
           </div>
         </div>
+
         <div class="relative">
           <input v-model="endLocation" type="text" name="endLocation" id="endLocation" class="pl-10 rounded-lg lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-600 focus:border-green-600 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Wernigerode" required="">
           <div class="absolute inset-y-4 start-0 top-0 flex items-center ps-3.5 pt-0 pointer-events-none">
@@ -33,6 +35,7 @@
         <input v-model="startLocation" type="text" name="startLocation" id="startLocation" class="rounded-lg w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-600 focus:border-green-600 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Magdeburg" required="">
         <input v-model="endLocation" type="text" name="endLocation" id="endLocation" class="rounded-lg w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-600 focus:border-green-600 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Wernigerode" required="">
         -->
+
         <div class="relative">
           <select v-model="transportMediumName" :onchange="resetInputs" id="transportMediumName"
                   class="pl-10 rounded-lg lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -47,6 +50,7 @@
             <i class="fa-solid fa-car"></i>
           </div>
         </div>
+
         <!--
         <select v-model="transportMediumName" :onchange="resetInputs" id="transportMediumName"
                 class="rounded-lg w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -59,6 +63,7 @@
 
         </select>
         -->
+
         <div class="relative" v-if="isTransportMediumFuelAvailable()">
           <select v-model="transportMediumFuel" :onchange="resetSize" name="transportMediumFuel" id="transportMediumFuel"
                   class="pl-10 rounded-lg lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -73,6 +78,7 @@
             <i class="fa-solid fa-gas-pump"></i>
           </div>
         </div>
+
         <!--
         <select v-model="transportMediumFuel" :onchange="resetSize" v-if="isTransportMediumFuelAvailable()" name="transportMediumFuel" id="transportMediumFuel"
                 class="rounded-lg w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -84,6 +90,7 @@
           </option>
         </select>
         -->
+
         <div class="relative" v-if="isTransportMediumSizeAvailable()">
           <select v-model="transportMediumSize"  name="transportMediumSize" id="transportMediumSize"
                   class="pl-10 rounded-lg  lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -98,6 +105,7 @@
             <i class="fa-solid fa-up-down"></i>
           </div>
         </div>
+
         <!--
         <select v-model="transportMediumSize" v-if="isTransportMediumSizeAvailable()" name="transportMediumSize" id="transportMediumSize"
                 class="rounded-lg w-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-500 focus:border-green-500 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -109,6 +117,7 @@
           </option>
         </select>
         -->
+
         <div class="relative" v-if="isTransportMediumFuelConsumptionAvailable()">
           <input v-model="transportMediumFuelConsumption" type="text" name="consumption" id="consumption"
                  class="pl-10 rounded-lg lg:w-auto w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-green-600 focus:border-green-600 mb-4 me-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="7.2" required="">
@@ -116,16 +125,27 @@
             <i class="fa-solid fa-percent"></i>
           </div>
         </div>
+
         <!--
         <div v-if="isTransportMediumFuelConsumptionAvailable()">
           <input v-model="transportMediumFuelConsumption" type="text" name="consumption" id="consumption" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="7.2" required="">
         </div>
         -->
       </div>
+
+      <div v-if="true" class="col-span-2 flex justify-center lg:flex-row flex-col ">
+        <div class="relative lg:w-auto w-full pl-10 mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="startLocationInvalid">Erforderlich (start)</div> </div>
+        <div class="relative lg:w-auto w-full pl-10 mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="endLocationInvalid">Erforderlich (end)</div> </div>
+        <div class="relative lg:w-auto w-full pl-10 mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="transportMediumNameInvalid">Erforderlich (transName)</div> </div>
+        <div v-if="isTransportMediumFuelAvailable()" class="relative lg:w-auto w-full pl-10 mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="transportMediumFuelInvalid" >Erforderlich (transFuel)</div> </div>
+        <div v-if="isTransportMediumSizeAvailable()" class="relative lg:w-auto w-full pl-10 mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="transportMediumSizeInvalid" >Erforderlich (transSize)</div> </div>
+        <div v-if="isTransportMediumFuelConsumptionAvailable()" class="relative pl-10 lg:w-auto w-full mt-2 text-sm text-red-600 dark:text-red-500 relative"> <div v-if="transportMediumFuelConsumptionInvalid" >Muss eine gültige Zahl sein (CustomConsumption) <br/> (z.B. 12345.67)</div> </div>
+      </div>
+
     </div>
 
     <div class="sm:col-span-2 text-white flex items-center justify-end">
-      <button @click="resetForm" class="inline-flex items-center p-2 px-4 mx-2 text-sm font-medium border text-center text-gray-500 bg-white hover:bg-gray-100 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:text-gray-900">
+      <button @click="resetForm" class="inline-flex items-center p-2 px-4 mx-2 text-sm font-medium text-center text-white bg-orange-400 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-orange-600">
         Eingaben zurücksetzen
       </button>
       <button type="submit" class="inline-flex items-center p-2 px-4 text-sm font-medium text-center text-white bg-green-500 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-green-800">
@@ -133,16 +153,13 @@
       </button>
     </div>
   </form>
-
 </template>
 
 <script>
 import api from "../api/api";
-import SaveEmissionModal from "./SaveEmissionModal.vue";
 
 export default {
   name: "Calculation",
-  components: {SaveEmissionModal},
   data() {
     return {
       startLocation: "",
@@ -153,6 +170,13 @@ export default {
       calculatedEmission: 0,
       transportMediumFuelConsumption: 0,
       transportMediumCustomConsumption: false,
+
+      startLocationInvalid: false,
+      endLocationInvalid: false,
+      transportMediumNameInvalid: false,
+      transportMediumFuelInvalid: false,
+      transportMediumSizeInvalid: false,
+      transportMediumFuelConsumptionInvalid: false,
     }
   },
   methods: {
@@ -164,6 +188,7 @@ export default {
       this.calculatedEmission = 0;
       this.transportMediumFuelConsumption = undefined;
       this.transportMediumCustomConsumption = false;
+      this.resetValidationError();
     },
     resetInputs(){
       this.resetFuel();
@@ -237,24 +262,26 @@ export default {
       return this.transportMediumCustomConsumption === true && this.transportMediumName !== 'default' && this.transportMediumFuel !== 'default';
     },
     validateInput() {
-      if(this.startLocation && this.endLocation) {
-        if(this.transportMediumName !== 'default') {
-          if(this.isTransportMediumSizeAvailable()) {
-            return this.transportMediumSize !== 'default';
-          }
-          if(this.isTransportMediumFuelAvailable()) {
-            return this.transportMediumFuel !== 'default';
-          }
-        } else return false;
-        return true;
-      }
-      return false;
+      this.startLocationInvalid = (this.startLocation === '');
+      this.endLocationInvalid = (this.endLocation === '');
+      this.transportMediumNameInvalid = (this.transportMediumName === 'default');
+      this.transportMediumSizeInvalid = (this.isTransportMediumSizeAvailable() && this.transportMediumSize === 'default');
+      this.transportMediumFuelInvalid = (this.isTransportMediumFuelAvailable() && this.transportMediumFuel === 'default');
+      this.transportMediumFuelConsumptionInvalid = (this.transportMediumCustomConsumption && isNaN(this.transportMediumFuelConsumption) || (this.transportMediumFuelConsumption == 0));
+      return !(this.startLocationInvalid || this.endLocationInvalid || this.transportMediumNameInvalid || this.transportMediumFuelConsumptionInvalid || this.transportMediumFuelInvalid || this.transportMediumSizeInvalid);
     },
-
+    resetValidationError(){
+      this.startLocationInvalid = false;
+      this.endLocationInvalid = false;
+      this.transportMediumNameInvalid = false;
+      this.transportMediumFuelConsumptionInvalid = false;
+      this.transportMediumFuelInvalid = false;
+      this.transportMediumSizeInvalid = false;
+    },
     async calculateEmission() {
       if(!this.validateInput()) {
-        console.error("Input invalid.")
-        console.error("startLocation and endLocation must not be empty.")
+        console.error("Input invalid.");
+        console.error("startLocation and endLocation must not be empty.");
         console.error("transportMediumName must not be null.");
         return;
       }
