4a86f22a981ac6aafbfe1571cfbca8d59f8bbb35
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-01-31 23:58:05 +0100

    removed unused code

--- src/App.vue
+++ src/App.vue
@@ -25,46 +25,13 @@ window.addEventListener("beforeunload", async function () {
 export default {
   name: "App",
   components: {Header, Footer},
-  data() {
-    return {
-
-    }
-  },
   async mounted() {
     sessionStorage.clear();
     if(!sessionStorage.startTime) {
       sessionStorage.startTime = Date.now();
     }
-    //window.addEventListener("beforeunload", this.handleLeaveEvent);
-    //window.addEventListener("beforeunload", this.beforeunload);
     console.log("mounted...");
-  },
-  //async beforeUnmount() {
-    //window.removeEventListener("beforeunload", this.handleLeaveEvent());
-  //},
-  methods: {
-    async handleLeaveEvent() {
-      console.log("handleLeaveEvent...");
-      let currentTime = Date.now();
-      let startTime = sessionStorage.startTime || currentTime;
-      let elapsedTime = currentTime - startTime;
-      if (!sessionStorage.visitorTotalTime) {
-        sessionStorage.visitorTotalTime = 0;
-      }
-      let totalDuration = parseInt(sessionStorage.visitorTotalTime) + elapsedTime;
-      sessionStorage.visitorTotalTime = totalDuration;
-
-      try {
-        const stats = await api.sendStatistic();
-        console.log(stats);
-      } catch (error) {
-        console.error("Error sending statistics: ", error);
-      }
-    },
-    beforeunload() {
-      this.handleLeaveEvent();
-    }
-  },
+  }
 }
 
 </script>
